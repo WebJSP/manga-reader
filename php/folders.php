45fa7974f0028c71219f3c1d62a700cacb166b58
Massimo Perin <m.perin@clivet.it>
2014-10-30 14:37:44 +0100

    fix: wrong path
    the pages was generating a wrong path.

--- php/folders.php
+++ php/folders.php
@@ -1,19 +1,1 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-error_reporting(0);
-
-$folder = $_POST["folder"];
-if (!isset($folder)) {
-	$folder = $_GET["folder"];
-}
-$folders = array();
-foreach (new DirectoryIterator($folder) as $fileInfo) {
-    if($fileInfo->isDot()) 
-    	continue;
-    if(!$fileInfo->isDir()) 
-    	continue;
-    $folders[] = $folder.'/'.$fileInfo->getFilename();
-}
-sort($folders, SORT_STRING);
-echo json_encode($folders);
-?>
+<?phpheader('Content-Type: application/json; charset=utf-8');error_reporting(0);$folder = filter_input(INPUT_POST, "folder");if (!isset($folder)) {    $folder = filter_input(INPUT_GET, "folder");}if (isset($folder)) {    $folders = array();    foreach (new DirectoryIterator('../'.$folder) as $dirInfo) {        if($dirInfo->isDot())             continue;        if(!$dirInfo->isDir())             continue;        $path = $folder.'/'.$dirInfo->getFilename();        $files = array();        foreach (new DirectoryIterator('../'.$path) as $fileInfo) {            if($fileInfo->isDot() || !$fileInfo->isFile())                 continue;            if($fileInfo->getExtension()=='json')                continue;            $files[] = $path.'/'.$fileInfo->getFilename();        }        sort($files, SORT_STRING);        $info = json_decode(utf8_encode(file_get_contents('../'.$path."/000_000.json")), true);        $contents = array();        $contents["path"]=$path;        $contents["files"]=$files;        $contents["info"]=$info;        $folders[] = $contents;    }    sort($folders, SORT_STRING);    echo json_encode($folders);} else {    echo "[]";}?>
