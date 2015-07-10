581025003a5881046052fb965efc09de51e8446e
Massimo Perin <m.perin@clivet.it>
2015-07-10 15:26:16 +0200

    removed parse.com test

--- js/blog.js
+++ js/blog.js
@@ -18,10 +18,5 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("asBFKXw6alVzEkNvqPAtkKeHNMJ50m5lpibJ4Tax", "pGrNOksA1YKtW48YS0jRaOmwsm19jVYd8MyFY7J9");
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
  
 });
