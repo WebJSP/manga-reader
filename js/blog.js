a6cb6a551bcf0e0ed5843767bdbee3c147f48a46
Massimo Perin <m.perin@clivet.it>
2015-07-17 14:47:44 +0200

    first try to read and show posts

--- js/blog.js
+++ js/blog.js
@@ -1,18 +1,18 @@
-/* global Parse */
+/* global Parse, Handlebars */
 
 $(function() {
     Array.prototype.forEach.call(document.querySelectorAll('.mdl-card__media'), function(el) {
-      var link = el.querySelector('a');
-      if(!link) {
-        return;
-      }
-      var target = link.getAttribute('href');
-      if(!target) {
-        return;
-      }
-      el.addEventListener('click', function() {
-        location.href = target;
-      });
+        var link = el.querySelector('a');
+        if(!link) {
+            return;
+        }
+        var target = link.getAttribute('href');
+        if(!target) {
+            return;
+        }
+        el.addEventListener('click', function() {
+            location.href = target;
+        });
     });
  
     Parse.$ = jQuery;
@@ -23,10 +23,20 @@ $(function() {
     var Blogs = Parse.Collection.extend({
         model: Blog
     });
+    var BlogsView =  Parse.View.extend({
+        template: Handlebars.compile($('#blogs-tpl').html()),
+        render: function(){ 
+            var collection = { blog: this.collection.toJSON() };
+            this.$el.html(this.template(collection));
+        }
+    });    
+    
     var blogs = new Blogs();
     blogs.fetch({
         success: function(blogs) {
-            console.log(blogs);
+            var blogsView = new BlogsView({ collection: blogs });
+            blogsView.render();
+            $('.demo-blog__posts').html(blogsView.el);
         },
         error: function(blogs, error) {
             console.log(error);
